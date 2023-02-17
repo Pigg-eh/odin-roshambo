a5e5c865d065285c2533c5f07446ba2ad9bde589
Hunter <ep.hunter23@gmail.com>
2023-02-17 13:55:25 -0800

    Add instructions to the code

---
+++ roshambo.js
@@ -0,0 +1,13 @@
+console.log ("So it begins")
+/* get getComputerChoice => randomly returns rock paper or scissors
+ (debug to make sure it is working properly)
+
+ function that plays a single round of roshambo. 2 parameters playerSelection(case 
+insensitive) and computerSelection- returns a string that declares the winner of the round
+
+return the results of fuction call not console.lgo
+
+write new function called game(). Call the playRound function inside 
+this one to play a 5 round game that keeps score and reports a winner
+or loser at the end.
+*/
